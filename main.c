a2ef534ff4ee0156caed69566a3b5d897ba09a00
Ruben Mikkonen <camu.umac@gmail.com>
2011-12-29 19:51:07 +0200

    huijauskoodi lisätty

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int gameloop( );
 
 #define __default_pf "def.pf"
 
+//#define cheat_code
+#ifdef cheat_code
+	char cheat = 0;
+#endif
+
 char *pf;
 int w, h, score = 1;
 
@@ -30,7 +35,7 @@ int main( int argc, char *argv[] ) {
 	int end = gameloop( );
 
 	clear( );
-	if( end == 0 ) mvaddstr( 10, 10, "n00b, u lost the game" );
+	if( end == 0 ) mvaddstr( 10, 10, "n00b, u lost THE GAME" );
 	if( end == 1 ) mvprintw( 10, 10, "gz, u wind! pisteesi: %i", score );
 	if( end == 2 ) mvaddstr( 10, 10, "ONNEE! vedit 9001 pistettä eli sait\n          huippusalaisen supervoiton\n          t: naruto" );
 
@@ -39,6 +44,8 @@ int main( int argc, char *argv[] ) {
 	endwin( );
 
 	free( pf );
+
+	if( end < 2 ) { int i; for( i = 0; i < 50; i++ ) printf( "THE GAME\n" ); }
 
 	return 0;
 }
@@ -80,6 +87,11 @@ int gameloop( ) {
 				break;
 			case 'q':
 				q = 1;
+				break;
+#ifdef cheat_code
+			case '*':
+				if( score == 8001 ) cheat = 1;
+#endif
 		}
 
 		{ int j, i; for( j = 0; j < h; j++ ) for( i = 0; i < w; i++ ) mvaddch( j, i, pf[j*w+i] ); }
@@ -92,13 +104,14 @@ int gameloop( ) {
 				y++;
 				canjump = 1;
 				break;
-
 			case 'V':
 				return 1;
-
 			case '*':
+#ifndef cheat_code
 				return 0;
-
+#else
+				if( !cheat ) return 0;
+#endif
 			case '!':
 				pf[y*w+x] = ' ';
 				score += 1000;
